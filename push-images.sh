26d2a3e7f1d300d4764f17df00660aae25e6ffc4
root <root@ubuntu.members.linode.com>
2019-08-08 03:45:02 +0000

    init imgages aliyun dockerhub

--- push-images.sh
+++ push-images.sh
@@ -2,12 +2,12 @@
 #push google or coreos images to dockerhub or aliyun registry,
 #change the registry name and username/password to yourself's.
 
-DOCKER_HUB=willdockerhub
-ALI_REGISTRY=registry.cn-hangzhou.aliyuncs.com/aliwill
+DOCKER_HUB=l7946381/k8s
+ALI_REGISTRY=registry.cn-beijing.aliyuncs.com/will
 
 #config the following env at travis-ui
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker login registry.cn-hangzhou.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
+docker login registry.cn-beijing.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
 
 for image in $(cat img-list.txt)
 do
@@ -15,8 +15,10 @@ do
 	docker pull $image
 	docker tag $image $ALI_REGISTRY/$imagename
 	docker tag $image $DOCKER_HUB/$imagename
+
 	# push到dockerhub
 	docker push $DOCKER_HUB/$imagename
+
 	# push到阿里云仓库
 	docker push $ALI_REGISTRY/$imagename
 done
